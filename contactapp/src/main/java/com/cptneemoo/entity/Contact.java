3c272a29e1710546315ae8af18d5c38618db035b
Сергей <@GoldPC>
2021-02-18 06:13:06 +0100

    added contact validation size constraints

--- contactapp/src/main/java/com/cptneemoo/entity/Contact.java
+++ contactapp/src/main/java/com/cptneemoo/entity/Contact.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "Contact")
@@ -16,9 +17,13 @@ public class Contact {
 	private long id;
 	
 	@NotBlank(message = "Name is mandatory")
+	@Size(min = 2, max = 255, message 
+    = "Name must be between 2 and 255 characters")
 	private String name;
 	
 	@NotBlank(message = "Contact number is mandatory")
+	@Size(min = 3, max = 32, message 
+    = "Contact number must be between 3 and 32 characters")
 	private String contactnumber;
 	
 	public long getId() {
